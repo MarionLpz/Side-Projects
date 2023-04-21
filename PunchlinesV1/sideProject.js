43c7b8ef14595dbe2cf926e43467a2d4cb0d01e2
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-04-21 16:23:19 +0200

    Ajout projet PunchlinesV1

---
+++ PunchlinesV1/sideProject.js
@@ -0,0 +1,184 @@
+let punchlines = [
+    {
+        "idpunchlines": "#Perlimpinpin",
+        "auteur": "Emmanuel Macron",
+        "description": "\"Ce que vous proposez, c'est de la poudre de perlimpinpin\"",
+        "source": "Débat 2e tour campagne présidentielle",
+        "date": "2017-05-03",
+        "partiPolitique": "LREM",
+        "url": "https://www.youtube.com/embed/BwnFpZc-yO4?autoplay=1"
+    },
+    {
+        "idpunchlines": "#Jeanne",
+        "auteur": "Jean-Marie Le Pen",
+        "description": "\"Jeanne, au secours !\"",
+        "source": "Défilé du FN en hommage à Jeanne d'Arc",
+        "date": "2015-05-01",
+        "partiPolitique": "Extreme-droite",
+        "url": "https://www.youtube.com/embed/v1mxMtr8Mws?autoplay=1"
+    },
+    {
+        "idpunchlines": "#Racailles",
+        "auteur": "Nicolas Sarkozy",
+        "description": "\"Vous en avez assez de cette bande de racailles ? Et bah on va vous en débarasser\"",
+        "source": "Visite à Argenteuil en tant que ministre de l'Intérieur",
+        "date": "2005-10-25",
+        "partiPolitique": "Droite",
+        "url": "https://www.youtube.com/embed/OofzN2mnzjg?autoplay=1"
+    },
+    {
+        "idpunchlines": "#Republique",
+        "auteur": "Jean-Luc Mélenchon",
+        "description": "\"La république, c'est moi !\"",
+        "source": "Perquisition au siège de la France Insoumise",
+        "date": "2018-10-16",
+        "partiPolitique": "Gauche",
+        "url": "https://www.youtube.com/embed/Q5LZcw6MASA?autoplay=1"
+    },
+    {
+        "idpunchlines": "#QueueDuChien",
+        "auteur": "Valérie Pécresse",
+        "description": "\"Les sondages ça va, ça vient, c'est comme la queue du chien\"",
+        "source": "Meeting campagne présidentielle",
+        "date": "2022-03-22",
+        "partiPolitique": "Droite",
+        "url": "https://www.youtube.com/embed/Fi0ACZh9yps?autoplay=1"
+    },
+    {
+        "idpunchlines": "#WinTheYes",
+        "auteur": "Jean-Pierre Raffarin",
+        "description": "\"Win the yes needs the no to win against the no\"",
+        "source": "Premier ministre en campagne pour le 'Oui' au référendum pour la Constitution européenne",
+        "date": "2005-05-01",
+        "partiPolitique": "Droite",
+        "url": "https://www.youtube.com/embed/ZV3L2y7UwNQ?autoplay=1"
+    },
+    {
+        "idpunchlines": "#BeYou",
+        "auteur": "François Hollande",
+        "description": "\"Be you, be proud of you because you can be do what we want to do\"",
+        "source": "Discours présidentiel lors d'un voyage aux Philippines",
+        "date": "2015-02-27",
+        "partiPolitique": "SocDem",
+        "url": "https://www.youtube.com/embed/M2wyG8Kt3fA?autoplay=1"
+    },
+    {
+        "idpunchlines": "#ImmunitéOuvrière",
+        "auteur": "Philippe Poutou",
+        "description": "\"Nous on n'a pas d'immunité ouvrière\"",
+        "source": "Débat campagne présidentielle",
+        "date": "2017-04-05",
+        "partiPolitique": "Turbo-Gauche",
+        "url": "https://www.youtube.com/embed/2NdrnH64-a8?autoplay=1"
+    },
+    {
+        "idpunchlines": "#Propre",
+        "auteur": "Valérie Pécresse",
+        "description": "\"Moi je veux une région propre et rien de tel qu'une femme pour faire le ménage\"",
+        "source": "Campagne aux élections régionales",
+        "date": "2012-05-11",
+        "partiPolitique": "Droite",
+        "url": "https://www.youtube.com/embed/o3wTHOnMMSY?autoplay=1"
+    },
+    {
+        "idpunchlines": "#PauvCon",
+        "auteur": "Nicolas Sarkozy",
+        "description": "\"Casse toi pauvre con\"",
+        "source": "Président de la République en visite au salon de l'Agriculture",
+        "date": "2008-02-23",
+        "partiPolitique": "Droite",
+        "url": "https://www.youtube.com/embed/OofzN2mnzjg?autoplay=1"
+    },
+    {
+        "idpunchlines": "#Projeeet",
+        "auteur": "Emmanuel Macron",
+        "description": "\"Parce que c'est notre projeeeet !\"",
+        "source": "Meeting",
+        "date": "2016-12-10",
+        "partiPolitique": "LREM",
+        "url": "https://www.youtube.com/embed/udBPW2rMQnc?autoplay=1"
+    },
+    {
+        "idpunchlines": "#Craquage",
+        "auteur": "Olivier Dussopt",
+        "description": "\"Personne n'a craqué\"",
+        "source": "Assemblée Nationale",
+        "date": "2023-04-15",
+        "partiPolitique": "LREM",
+        "url": "https://www.youtube.com/embed/9Ehy2VblnQY?autoplay=1"
+    },
+    {
+        "idpunchlines": "#IlsSontPartout",
+        "auteur": "Marine Le Pen",
+        "description": "\"Ils sont là, ils sont partout, dans les villes, dans les campagnes...\"",
+        "source": "France2",
+        "date": "2017-05-03",
+        "partiPolitique": "Extreme-droite",
+        "url": "https://www.youtube.com/embed/ZOm5uTyjXxw?autoplay=1"
+    },
+    {
+        "idpunchlines": "#Cessez",
+        "auteur": "Jacques Chirac",
+        "description": "Soyez gentil de me laisser parler et de cesser d’intervenir incessamment comme le roquet\"",
+        "source": "Debat",
+        "date": "1985-10-27",
+        "partiPolitique": "Droite",
+        "url": "https://www.youtube.com/embed/HN1N_IyvjAQ?autoplay=1"
+    },
+    {
+        "idpunchlines": "#Arrêtez",
+        "auteur": "Edouard Balladur",
+        "description": "\"Je vous demande de vous arrêter\"",
+        "source": "Debat",
+        "date": "1995-04-23",
+        "partiPolitique": "Droite",
+        "url": "https://www.youtube.com/embed/V_4CZu4ghMo?autoplay=1"
+    }
+]
+
+//Attendre affichage du HTML avant lancement JS
+document.addEventListener("DOMContentLoaded", ready);
+function ready() {
+    boucle()
+}
+
+function triAleatoire() {
+    return Math.random() - Math.random()
+}
+
+//Fonction boucle pour parcourir le tableau des punchlines
+function boucle() {
+    punchlines.sort(triAleatoire)
+    for (let i = 0; i < punchlines.length; i++) {
+        let element = punchlines[i];
+        console.log(element)
+        document.getElementById("tags").innerHTML += getHtml(element)
+    }
+}
+
+//Affichage des tags idpunchlines
+function getHtml(punchline) {
+    return "<button id=" + punchline.idpunchlines + " onclick='onclickelement(this.id)'> " + punchline.idpunchlines + "</a >"
+}
+
+//Affichage au clic sur un bouton tag de la section player avec la vidéo punchline + détail
+function onclickelement(id) {
+    let punchline = punchlines.find(x => x.idpunchlines === id);
+    document.getElementById("player").innerHTML = getDetailHtml(punchline)
+    let player = document.getElementById("player")
+    player.style.visibility = "visible"
+}
+
+//Affichage du détail des punchlines
+function getDetailHtml(punchline) {
+    return '<iframe width="420" height="315" src="' + punchline.url + '" allow="autoplay"></iframe>' + "<br> <h5>" + punchline.idpunchlines + "</h5>" + punchline.description + "<br><br>" + punchline.auteur + "<br>" + punchline.source + "<br>" + punchline.date
+}
+
+//Fonction changement coleurs tags
+function ran_col() {
+    var color = "#"
+    var letters = ['d886a7ff', 'd7be82ff', '7a4419ff', '3b588eff', 'ce2d32ff'];
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('tags').style.background = color;
+}
+ran_col()
