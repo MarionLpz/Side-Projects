4f522aed7586658c75b8bac803fbd41e5980add3
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-07-18 22:54:25 +0200

    Création Footer + CSS

---
+++ React-Portfolio/src/components/Footer.js
@@ -0,0 +1,33 @@
+import { Col, Container, Row } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.png";
+import navIcon2 from "../assets/img/nav-icon2.png";
+import navIcon3 from "../assets/img/nav-icon3.png";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container className="text-container">
+        <Row className="align-item-center">
+          <Col sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="">
+                <img src={navIcon1} />
+              </a>
+              <a href="">
+                <img src={navIcon2} />
+              </a>
+              <a href="">
+                <img src={navIcon3} />
+              </a>
+            </div>
+            <p>CopyRight 2022. All right reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
