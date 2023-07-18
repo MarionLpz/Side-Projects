1ac7fc5e7adccf34730270b52825e51171f79a9c
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-07-18 22:31:52 +0200

    Création Contact(+ server) + CSS

--- React-Portfolio/src/components/Contact.js
+++ React-Portfolio/src/components/Contact.js
@@ -22,73 +22,97 @@ export const Contact = () => {
     });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+    let response = await fetch("http://localhost:3000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formDetails),
+    });
+    setButtonText("Send");
+    let result = response.json();
+    setFormDetails(formInitialDetails);
+    if (result.code === 200) {
+      setStatus({ success: true, message: "Message sent successfully" });
+    } else {
+      setStatus({
+        success: false,
+        message: "Something went wrong, please try again later.",
+      });
+    }
+  };
   return (
     <section className="contact" id="connect">
       <Container>
-        <Col md={6}>
-          <img src={contactImg} alt="Contact me">
-            {" "}
-          </img>
-        </Col>
-        <Col md={6}>
-          <h2>Get in touch</h2>
-          <form>
-            <Row>
-              <Col sm={6} className="px-1">
-                <input
-                  type="text"
-                  value={formDetails.firstName}
-                  placeholder="First Name"
-                  onChange={(e) => onFormeUpdate("firstName", e.target.value)}
-                />
-              </Col>
-              <Col sm={6} className="px-1">
-                <input
-                  type="text"
-                  value={formDetails.lastName}
-                  placeholder="Last Name"
-                  onChange={(e) => onFormeUpdate("lastName", e.target.value)}
-                />
-              </Col>
-              <Col sm={6} className="px-1">
-                <input
-                  type="email"
-                  value={formDetails.email}
-                  placeholder="Email"
-                  onChange={(e) => onFormeUpdate("email", e.target.value)}
-                />
-              </Col>
-              <Col sm={6} className="px-1">
-                <input
-                  type="tel"
-                  value={formDetails.phone}
-                  placeholder="Phone No."
-                  onChange={(e) => onFormeUpdate("phone", e.target.value)}
-                />
-              </Col>
-              <Col>
-                <textarea
-                  row="6"
-                  value={formDetails.message}
-                  placeholder="Message"
-                  onChange={(e) => onFormeUpdate("message", e.target.value)}
-                />
-                <button type="submit">
-                  <span>{buttonText}</span>
-                </button>
-              </Col>
-              {status.message && (
-                <Col>
-                  <p
-                    className={status.success === false ? "danger" : "success"}
-                  >
-                    {status.message}
-                  </p>
+        <Row className="align-items-center">
+          <Col md={6}>
+            <img src={contactImg} alt="Contact me" />
+          </Col>
+          <Col md={6}>
+            <h2>Get in touch</h2>
+            <form onSubmit={handleSubmit}>
+              <Row>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    value={formDetails.firstName}
+                    placeholder="First Name"
+                    onChange={(e) => onFormUpdate("firstName", e.target.value)}
+                  />
                 </Col>
-              )}
-            </Row>
-          </form>
-        </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    value={formDetails.lastName}
+                    placeholder="Last Name"
+                    onChange={(e) => onFormUpdate("lastName", e.target.value)}
+                  />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="email"
+                    value={formDetails.email}
+                    placeholder="Email"
+                    onChange={(e) => onFormUpdate("email", e.target.value)}
+                  />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="tel"
+                    value={formDetails.phone}
+                    placeholder="Phone No."
+                    onChange={(e) => onFormUpdate("phone", e.target.value)}
+                  />
+                </Col>
+                <Col>
+                  <textarea
+                    row="6"
+                    value={formDetails.message}
+                    placeholder="Message"
+                    onChange={(e) => onFormUpdate("message", e.target.value)}
+                  />
+                  <button type="submit">
+                    <span>{buttonText}</span>
+                  </button>
+                </Col>
+                {status.message && (
+                  <Col>
+                    <p
+                      className={
+                        status.success === false ? "danger" : "success"
+                      }
+                    >
+                      {status.message}
+                    </p>
+                  </Col>
+                )}
+              </Row>
+            </form>
+          </Col>
+        </Row>
       </Container>
     </section>
   );
