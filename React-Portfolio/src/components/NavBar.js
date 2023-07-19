7045011bef5b9d9600fdc96a16cacf2436ec571b
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-07-19 14:20:10 +0200

    Navbar content update

--- React-Portfolio/src/components/NavBar.js
+++ React-Portfolio/src/components/NavBar.js
@@ -73,13 +73,10 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#">
+                <a href="https://www.linkedin.com/in/marion-lopez-/">
                   <img src={navIcon1} alt="" />
                 </a>
-                <a href="#">
-                  <img src={navIcon2} alt="" />
-                </a>
-                <a href="#">
+                <a href="https://github.com/MarionLpz">
                   <img src={navIcon3} alt="" />
                 </a>
               </div>
